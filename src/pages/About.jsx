12895f6f459c8175ea8538061a5a0cd6c293637f
Big-pun <150970603+Big-pun@users.noreply.github.com>
2024-10-14 12:13:53 -0400

    home & about resposive

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,14 +1,19 @@
 import React, { Suspense, lazy } from 'react';
 import Avatar from '../Images/Avatar.png';
+import { MdOutlineKeyboardDoubleArrowDown } from "react-icons/md";
 
 const Icons = lazy(() => import('../components/About/Icons'));
 const H1About = lazy(() => import('../components/About/H1About'));
+
+const scrollToTextSection = () => {
+  document.getElementById('section2').scrollIntoView({ behavior: 'smooth' })
+};
 
 function About() {
   return (
     <div className='container mx-auto p-4 '>
       <section id='section1' className='grid grid-cols-12 md:min-h-screen'>
-        <div className='col-span-12 md:col-span-5 md:col-start-2'>
+        <div className='col-span-12 md:col-span-6 md:col-start-2'>
           <Suspense fallback={<div>Loading...</div>}>
             <H1About />
           </Suspense>
@@ -18,10 +23,20 @@ function About() {
           <img src={Avatar} alt="Avatar" className='rounded-2xl' />
         </div>
 
+        <div className='col-span-12 text-center hidden lg:block -mt-36'>
+          <button
+            className='text-green-500 text-5xl md:text-9xl animate-pulse items-center'
+            aria-label="Scroll to section 2"
+            onClick={scrollToTextSection}
+          >
+            <MdOutlineKeyboardDoubleArrowDown />
+          </button>
+        </div>
+
       </section>
 
-      <section id='section2' className='grid grid-cols-12 min-h-screen -mb-16'>
-        <div className='col-span-12 md:col-span-6 md:col-start-4'>
+      <section id='section2' className='grid grid-cols-12 md:min-h-screen pt-8 lg:-my-16'>
+        <div className='col-span-12 md:col-span-10 md:col-start-2 xl:col-span-8 xl:col-start-3'>
           <h2 className='text-5xl mb-12 md:mb-8'>
             What I'm <span className='text-green-500 font-bold'>Focusing</span> on
           </h2>
@@ -31,10 +46,10 @@ function About() {
             <span className='text-green-500 font-bold'> code</span> that ensures both high
             <span className='text-green-500 font-bold'> readability</span> and
             <span className='text-green-500 font-bold'> maintainability</span>.</p>
-            <br />
-            <p className='text-xl md:text-2xl mb-8'>
-              Additionally, I'm expanding my expertise into full-stack development, with a focus on Node.js and Next.js for server-side rendering and efficient back-end logic, as well as database management with MySQL and PHP to deliver comprehensive, scalable solutions.
-            </p>
+          <br />
+          <p className='text-xl md:text-2xl mb-4'>
+            Additionally, I'm expanding my expertise into full-stack development, with a focus on Node.js and Next.js, as well as database management with MySQL and PHP.
+          </p>
           <Suspense fallback={<div>Loading...</div>}>
             <Icons />
           </Suspense>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Dev from '../Images/Dev.svg';
 import { MdOutlineKeyboardDoubleArrowDown } from "react-icons/md";
 import H1 from '../components/Home/H1';
 import H2 from '../components/Home/H2';
+import { RiGitRepositoryCommitsLine } from "react-icons/ri";
 
 function Home() {
 
@@ -13,8 +14,8 @@ function Home() {
 
     return (
         <div className='container mx-auto p-4'>
-            <section className='grid grid-cols-12 min-h-screen'>
-                <div className='col-span-12 md:col-span-5 md:col-start-2'>
+            <section className='grid grid-cols-12 lg:min-h-screen'>
+                <div className='col-span-12 md:col-span-7 md:col-start-2'>
                     <H1 />
                     <div className='relative'>
                         <div className='h-16 md:h-20'>
@@ -28,7 +29,7 @@ function Home() {
                     <img src={Dev} alt="Dev" />
                 </div>
 
-                <div className='col-span-12 text-center hidden md:block -mt-36'>
+                <div className='col-span-12 text-center hidden lg:block -mt-36'>
                     <button
                         className='text-green-500 text-5xl md:text-9xl animate-pulse items-center'
                         aria-label="Scroll to section 2"
@@ -39,8 +40,8 @@ function Home() {
                 </div>
             </section>
 
-            <section id='section2' className='grid grid-cols-12 min-h-screen pt-8 md:-my-16'>
-                <div className='col-span-12 md:col-span-6 md:col-start-4'>
+            <section id='section2' className='grid grid-cols-12 md:min-h-screen pt-8 lg:-my-16'>
+                <div className='col-span-12 md:col-span-10 md:col-start-2 xl:col-span-8 xl:col-start-3'>
                     <h2 className='text-5xl mb-8'>
                         How This <span className='text-green-500 font-bold'>Portfolio</span> Was Built
                     </h2>
@@ -52,15 +53,18 @@ function Home() {
                         <li><span className='font-bold text-green-500'>Tailwind CSS:</span> Tailwind CSS is used for styling, providing a utility-first approach that allows for rapid and responsive development.</li>
                         <li><span className='font-bold text-green-500'>Vite:</span> Vite is used as the build tool, offering fast development and optimized production builds.</li>
                     </ul>
-                    <p className='text-xl md:text-2xl'>
+                    <p className='text-xl md:text-2xl mb-8'>
                         The combination of these technologies allows for a seamless and efficient development process, resulting in a portfolio that is both visually appealing and highly functional.
                     </p>
-                </div>
 
-                <div className='col-span-12 md:col-span-6 md:col-start-4'>
-                    <p className='text-xl md:text-2xl'>
+                    <p className='text-lg md:text-xl'>
                         Feel free to explore the code and reach out if you have any questions or suggestions!
                     </p>
+                    <div className="flex justify-center items-center">
+                        <a className='text-2xl md:text-4xl mx-auto text-center bg-green-500 px-4 my-4 border-2 border-gray-300 rounded-lg text-black animate-pulse' href='https://github.com/Big-pun/my-portfolio' target='_blank'>
+                            <RiGitRepositoryCommitsLine />
+                        </a>
+                    </div>
                 </div>
             </section>
         </div>
