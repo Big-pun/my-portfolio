fbdd0f8c4e8d8d78d9e1800d54f76b689f81168f
Big-pun <150970603+Big-pun@users.noreply.github.com>
2024-11-11 22:42:32 -0500

    appel api github languages camembert

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -2,6 +2,7 @@ import { Suspense, lazy } from 'react'
 import H1Work from '../components/Work/H1Work'
 import Projects from '../components/Work/Projects'
 import WebApps from '../components/Work/WebApps'
+import Repos from '../components/Work/Repos'
 
 const Github = lazy(() => import('../components/Work/Github'))
 
@@ -23,15 +24,19 @@ function Work() {
           </div>
 
           <div className='col-span-12 md:col-span-10 md:col-start-2 p-4'>
-
             <h2 className='text-4xl mb-4'>Days of <span className='text-green-500 font-bold'>Code</span></h2>
-
-              <Suspense fallback={<div>Loading...</div>}>
-                <Github />
-              </Suspense>
-            
-
+            <Suspense fallback={<div>Loading...</div>}>
+              <Github />
+            </Suspense>
           </div>
+
+          <div className='col-span-12 md:col-span-10 md:col-start-2 p-4'>
+            <h2 className='text-4xl mb-4'>Github <span className='text-green-500 font-bold'>Repositories</span></h2>
+            <Suspense fallback={<div>Loading...</div>}>
+              <Repos />
+            </Suspense>
+          </div>
+
         </div>
       </section>
     </div>
