e581e9b4b0f83330f366fa41deb92082cdc704d0
Big-pun <150970603+Big-pun@users.noreply.github.com>
2024-11-29 14:11:29 -0500

    test

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve, dirname } from 'path'
+import { fileURLToPath } from 'url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my-portfolio/'
+  base: '/my-portfolio/',
+  build: {
+    rollupOptions: {
+        main: resolve(dirname(fileURLToPath(import.meta.url)), "index.html"),
+        404: resolve(dirname(fileURLToPath(import.meta.url)), "public/404.html"),
+    }
+  }
 })
+
+
