a13ad24ac9f815c35e84aa72b5709e2ca0e19229
Big-pun <150970603+Big-pun@users.noreply.github.com>
2024-12-02 23:45:16 -0500

    deploy

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,18 +1,6 @@
-import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
 
 function ErrorPage() {
-  const navigate = useNavigate();
 
-  useEffect(() => {
-    // Rediriger après 3 secondes
-    const timer = setTimeout(() => {
-      navigate("/"); // Redirige vers la page d'accueil
-    }, 3000);
-
-    // Nettoyer le timer lorsque le composant est démonté
-    return () => clearTimeout(timer);
-  }, [navigate]);
 
   return (
     <div>
