ddbadf14d9dbab929a33912dc036d7f66730a84e
Big-pun <150970603+Big-pun@users.noreply.github.com>
2024-12-02 23:59:50 -0500

    essai error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,6 +1,20 @@
+import { useEffect } from "react";
+import { useNavigate, matchPath } from "react-router-dom";
+import myRoutes from "../routes/routes"; 
 
 function ErrorPage() {
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    const currentPath = window.location.pathname;
+    
+    const isValidPath = myRoutes.routes.some(route => matchPath(route.path, currentPath));
 
+
+    if (!isValidPath) {
+      navigate("/"); 
+    }
+  }, [navigate]);
 
   return (
     <div>
